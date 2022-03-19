7c98b500c86b31976c4c0254cc806b36b7507e2c
Alexandra Sirois <asirois9191@gmail.com>
2022-03-19 17:20:25 -0400

    sign-in ajouter avec firebase

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,5 +1,8 @@
 import Home from './routes/home/home.component';
 import Navigation from './routes/navigation/navigation.component';
+import SignIn from './routes/sign-in/sign-in.component';
+
+
 import { Routes, Route } from 'react-router-dom'
 
 
@@ -13,6 +16,7 @@ const App = () => {
     <Route path='/' element={<Navigation />}>
       <Route index element={<Home />}/>
       <Route path='shop' element={<Shop />}/>
+      <Route path='signIn' element={<SignIn />}/>
     </Route>
   </Routes>
   );
